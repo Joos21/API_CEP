0c532249544f228da9884e0f71ecaeefdfe225a8
Jose_Freires <88195769+Joos21@users.noreply.github.com>
2022-03-10 16:36:27 -0300

    Commit inicial.

---
+++ script.js
@@ -0,0 +1,29 @@
+const preencherForm = (end) =>{
+    document.getElementById('rua').value = end.logradouro
+    document.getElementById('bairro').value = end.bairro
+    document.getElementById('cidade').value = end.localidade
+    document.getElementById('estado').value = end.uf
+}
+
+const verificaCPF = async() => {
+
+    const cep = document.getElementById('cep').value;
+
+    if(cep != ""){
+        const url = `http://viacep.com.br/ws/${cep}/json/`;
+        
+        const dados = await fetch(url);
+        const end = await dados.json();
+
+        console.log(end)
+
+        preencherForm(end);
+    }
+    else{
+        document.getElementById('rua').value = ('')
+        document.getElementById('bairro').value = ('')
+        document.getElementById('cidade').value = ('')
+        document.getElementById('estado').value = ('')
+    }
+
+}
